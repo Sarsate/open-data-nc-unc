baeb1642358e026020091a2096204d8b125a0319
Nathaniel <nvspreck@gmail.com>
2013-12-08 01:42:16 +0000

    Bounties now have to be made for after today.
    See http://173.230.133.53:8444/#/c/12/ for how this was previously added.
    
    Change-Id: Ia48a7c5e54c4bb9b9f1637e10cf1a2f5295e36b5

--- open-data-nc/opendata/requests/forms.py
+++ open-data-nc/opendata/requests/forms.py
@@ -5,6 +5,12 @@ from selectable.forms import AutoCompleteSelectWidget
 from opendata.catalog.lookups import CityLookup, CountyLookup
 from .models import Request, Bounty, Resource
 
+from django.core.exceptions import ValidationError
+
+import datetime
+import re
+
+import types
 
 class SearchForm(forms.Form):
     text = forms.CharField(required=False)
@@ -47,6 +53,16 @@ class BountyForm(forms.ModelForm):
         js = (
             "suggestions/js/form.js",
         )
+
+    def clean(self):
+        cleaned_data = super(BountyForm, self).clean()
+        due_date = cleaned_data.get("deadline")
+        if due_date and due_date < datetime.date.today():
+            raise forms.ValidationError('Deadline must be in the future in the format yyyy-mm-dd')
+        bounty = cleaned_data.get("price")
+        if bounty < 0:
+            raise forms.ValidationError('Bounty must be a positive value between 0 and 1000 (ex. 10.50)')
+        return cleaned_data
 
 
 class SupplyForm(forms.ModelForm):
